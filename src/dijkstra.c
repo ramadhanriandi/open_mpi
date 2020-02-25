147c70741067aef3cc4f5dc4ed2b2053f981b806
ramadhanriandi <izurbudianto@gmail.com>
2020-02-25 15:54:11 +0700

    init mpi b_cast structure

--- src/dijkstra.c
+++ src/dijkstra.c
@@ -1,8 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
 // #include <mpi.h>
+#include <assert.h>
 
 int main(int argc, char** argv) {
+    // if (argc != 3) {
+    //     fprintf(stderr, "Usage: compare_bcast num_elements num_trials\n");
+    //     exit(1);
+    // }
+    // int num_elements = atoi(argv[1]);
+    // int num_trials = atoi(argv[2]);
+    // MPI_Init(NULL, NULL);
+    // int world_rank;
+    // MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+    // double total_mpi_bcast_time = 0.0;
+    // int i;
+    // int matrix_distance[8][8];
+    // init_graph(8, matrix_distance, 80);
+    // // int* data = (int*)malloc(sizeof(int) * num_elements);
+    // // assert(data != NULL);
+    // for (i = 0; i < num_trials; i++) {
+    //     // Time MPI_Bcast
+    //     MPI_Barrier(MPI_COMM_WORLD);
+    //     total_mpi_bcast_time -= MPI_Wtime();
+    //     MPI_Bcast(matrix_distance, num_elements, MPI_INT, 0, MPI_COMM_WORLD);
+    //     if (world_rank != 0) {
+    //         print_matrix(8, matrix_distance);
+    //     }
+    //     MPI_Barrier(MPI_COMM_WORLD);
+    //     total_mpi_bcast_time += MPI_Wtime();
+    // }
+    // // Print off timing information
+    // if (world_rank == 0) {
+    //     // printf("Data size = %d, Trials = %d\n", num_elements * (int)sizeof(int),
+    //     // num_trials);
+    //     printf("Avg MPI_Bcast time = %lf\n", total_mpi_bcast_time / num_trials);
+    // }
+    // MPI_Finalize();
+
     int n_node;
     int seed = 80;
 
@@ -16,7 +51,7 @@ int main(int argc, char** argv) {
 
     print_matrix(n_node, matrix_distance);
 
-    // this process must be made to be parallel
+    // THIS PROCESS MUST BE MADE TO BE PARALLEL WITH OPENMPI
     for (int i = 0; i < n_node; i++) {
         fill_array(n_node, result, -1);
         dijkstra(n_node, matrix_distance, i, result);
